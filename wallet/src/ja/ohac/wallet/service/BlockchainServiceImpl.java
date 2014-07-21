8ec9e2621fc7dc1fbb269adef4c046d532d2b150
OHASHI Hideya <ohachige@gmail.com>
2014-07-21 09:08:23 +0900

    remove unused IRC discovery

--- wallet/src/ja/ohac/wallet/service/BlockchainServiceImpl.java
+++ wallet/src/ja/ohac/wallet/service/BlockchainServiceImpl.java
@@ -410,7 +410,6 @@ public class BlockchainServiceImpl extends android.app.Service implements Blockc
 					int i = rand.nextInt(50);
 					String channel = "#sakuracoin" + String.format("%02d", i);
 					private final PeerDiscovery normalPeerDiscovery = new DnsDiscovery(Constants.NETWORK_PARAMETERS);
-					//private final PeerDiscovery normalPeerDiscovery = new IrcDiscovery(channel); // TODO
 					private final PeerDiscovery seedPeers = new SeedPeers(Constants.NETWORK_PARAMETERS);
 
 					@Override
